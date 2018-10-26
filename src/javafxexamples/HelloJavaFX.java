ba0c86a6fd1e58fe1c1d455ef5f36f14da1b8f3d
danielcregg <daniel.cregg@gmail.com>
2018-10-26 09:52:25 +0100

    Initial Commit - Set the Stage

---
+++ src/javafxexamples/HelloJavaFX.java
@@ -0,0 +1,19 @@
+package javafxexamples;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+// https://youtu.be/EEcpMTpaWhs
+public class HelloJavaFX extends Application {
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.show();
+		
+	}
+
+}
