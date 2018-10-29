17e97f05f98da375945f4824dc0c2e51c38f5399
danielcregg <daniel.cregg@gmail.com>
2018-10-29 18:56:40 +0000

    Add Leaf node blue rectangle

--- src/javafxexamples/SceneGraphDemo.java
+++ src/javafxexamples/SceneGraphDemo.java
@@ -4,20 +4,26 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
- 
+
 public class SceneGraphDemo extends Application {
- 
-   @Override
-   public void start(Stage stage) {
-       Group root = new Group();
-       Scene scene = new Scene(root, 500, 500, Color.BLACK);
-       stage.setTitle("JavaFX Scene Graph Demo");
-       stage.setScene(scene);
-       stage.show();
-   }
- 
-   public static void main(String[] args) {
-       launch(args);
-   }
+
+	@Override
+	public void start(Stage stage) {
+		Group root = new Group();
+		Scene scene = new Scene(root, 500, 500, Color.BLACK);
+
+		Rectangle r = new Rectangle(25, 25, 250, 250);
+		r.setFill(Color.BLUE);
+		root.getChildren().add(r);
+
+		stage.setTitle("JavaFX Scene Graph Demo");
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
