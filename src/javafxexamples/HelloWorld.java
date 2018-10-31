bd58c516b9ccc2a6a9adac791ed62452bc54ee46
danielcregg <daniel.cregg@gmail.com>
2018-10-31 16:48:16 +0000

    Add a label, a text field and a button

--- src/javafxexamples/HelloWorld.java
+++ src/javafxexamples/HelloWorld.java
@@ -3,8 +3,10 @@ package javafxexamples;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -20,6 +22,13 @@ public class HelloWorld extends Application{
 		Text text2 = new Text(0, 20, "Hello World2");
 		Text text3 = new Text(0, 30, "Hello World3");
 		text3.setFill(Color.BLUE);
+		Label label = new Label("This is a label");
+		label.setTranslateY(30);
+		TextField textField = new TextField("This is a text field");
+		textField.setTranslateY(50);
+		Button button = new Button("Click Me!");
+		button.setTranslateY(80);
+		
 		
 		// Create a Root Node for our Scene Graph
 		Group root = new Group();
@@ -28,6 +37,9 @@ public class HelloWorld extends Application{
 		root.getChildren().add(text1);
 		root.getChildren().add(text2);
 		root.getChildren().add(text3);
+		root.getChildren().add(label);
+		root.getChildren().add(textField);
+		root.getChildren().add(button);
 		
 		// Assigning the scene
 		Scene scene = new Scene(root, sceneWidth, sceneHeight); // Must specify the root Node for the scene graph
