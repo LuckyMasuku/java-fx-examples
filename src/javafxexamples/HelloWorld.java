4b348aafbd1ceac0d501abd52415e1fc455d0c45
danielcregg <daniel.cregg@gmail.com>
2018-10-31 16:29:50 +0000

    Add root Node, Text node and Scene

--- src/javafxexamples/HelloWorld.java
+++ src/javafxexamples/HelloWorld.java
@@ -1,14 +1,30 @@
 package javafxexamples;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class HelloWorld extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		// Create a Text node
+		Text text = new Text(0, 10, "Hello World");
+		
+		// Create a Root Node for our Scene Graph
+		Group root = new Group();
+		
+		// Add the text node to our root node
+		root.getChildren().add(text);
+		
+		// Assigning the scene
+		Scene scene = new Scene(root); // Must specify the root Node for the scene graph
 		// Setting the title for the primary Stage
 		primaryStage.setTitle("Title for Primary Stage");	
+		// Set the scene
+		primaryStage.setScene(scene);
 		// Show the primary stage
 		primaryStage.show();
 	}
