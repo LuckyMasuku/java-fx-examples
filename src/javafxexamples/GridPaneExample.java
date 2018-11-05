cab8c7aa0b47d84b2e53c7e9cfc5b490c57197ec
danielcregg <daniel.cregg@gmail.com>
2018-11-05 15:26:26 +0000

    Added Event handlers for buttons 2 and 3

--- src/javafxexamples/GridPaneExample.java
+++ src/javafxexamples/GridPaneExample.java
@@ -36,8 +36,8 @@ public class GridPaneExample extends Application {
 		
 		// Register event handler using Lambda Expression (JDK 8)
 		button1.setOnAction(event -> text1.setText("Button 1 Pressed"));
-		
-		
+		button2.setOnAction(event -> text2.setText("Button 2 Pressed"));
+		button3.setOnAction(event -> text3.setText("Button 3 Pressed"));
 		
 		Scene scene = new Scene(gridPane, 500, 100);
 		primaryStage.setScene(scene);
