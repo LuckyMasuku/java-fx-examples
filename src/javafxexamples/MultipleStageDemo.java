6920b71b6aa11b84305d5b27ffaf8b04890de098
danielcregg <daniel.cregg@gmail.com>
2018-11-05 14:50:41 +0000

    Added button to close Window 2 and print to console

--- src/javafxexamples/MultipleStageDemo.java
+++ src/javafxexamples/MultipleStageDemo.java
@@ -11,10 +11,10 @@ public class MultipleStageDemo extends Application {
 		Stage secondaryStage = new Stage(); // Create a secondary stage
 
 		Button button1 = new Button("Open Secondary Stage");
-
+		Button button2 = new Button("Scene2 Button");
 		// Create two scenes and place a button in each scene
 		Scene scene1 = new Scene(button1, 300, 300);
-		Scene scene2 = new Scene(new Button("Scene2 Button"), 250, 250);
+		Scene scene2 = new Scene(button2, 250, 250);
 
 		primaryStage.setTitle("Primary Stage"); // Set the stage title
 		primaryStage.setScene(scene1); // Place the scene in the stage
@@ -23,6 +23,10 @@ public class MultipleStageDemo extends Application {
 		secondaryStage.setTitle("Secondary Stage"); // Set the second stage title
 		secondaryStage.setScene(scene2); // Place the scene in the stage
 		button1.setOnAction(event -> secondaryStage.show());
+		button2.setOnAction(event -> {
+				secondaryStage.close();
+				System.out.println("Closed Window 2");
+			});
 	}
 
 	public static void main(String[] args) {
