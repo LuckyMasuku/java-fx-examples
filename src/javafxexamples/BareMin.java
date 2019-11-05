76004cc716b70722a35da41f125c6726bdef5f7b
danielcregg <daniel.cregg@gmail.com>
2019-11-05 11:38:20 +0000

    Added bare min project for next year

---
+++ src/javafxexamples/BareMin.java
@@ -0,0 +1,25 @@
+package javafxexamples;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+//import javafx.scene.Scene;
+//import javafx.scene.control.Label;
+//import javafx.scene.layout.Pane;
+
+public class BareMin extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+//		Label label1 = new Label("This is a label");
+//		Pane root = new Pane();
+//		root.getChildren().add(label1);
+//		Scene scene1 = new Scene(root);
+//		primaryStage.setScene(scene1);
+		primaryStage.show();
+	}
+
+	// Do not need main method with eclipse but should put it in
+//	public static void main(String[] args) {
+//		launch(args);
+//	}
+}
